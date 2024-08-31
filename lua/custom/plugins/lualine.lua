6f408bf4f60b4c66f22bdc28e9c5e33b14e5f0b0
Alvaro <ninobaldo@gmail.com>
2024-08-31 11:52:02 -0300

    Improve lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -4,6 +4,28 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function ()
-   require('lualine').setup({})
+    require('lualine').setup({
+
+      options = {
+        icons_enabled = true,
+        component_separators = '|',
+        section_separators = '',
+      },
+      sections = {
+        lualine_x = {
+          {
+            color = { fg = "#ff9e64" },
+          },
+          {
+            color = { fg = "#ff9e64" },
+          },
+        },
+        lualine_a = {
+          {
+            'buffers',
+          }
+        }
+      }
+    })
   end
 }
